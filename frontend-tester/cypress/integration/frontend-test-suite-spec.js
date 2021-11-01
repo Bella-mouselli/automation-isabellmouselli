ac6608d2c8abcda47d8e7349264e3266fd00447a
Isabell Mouselli <bellam95@hotmail.com>
2021-11-01 13:49:32 +0100

    all test cases runs

--- frontend-tester/cypress/integration/frontend-test-suite-spec.js
+++ frontend-tester/cypress/integration/frontend-test-suite-spec.js
@@ -2,52 +2,59 @@
 
 before(() => {//visit localhost3000
 
-    cy.visit('http://localhost:3000/%27')
+    cy.visit('http://localhost:3000')
 
   })
   // en test suite
-describe('Frontend test suite', function(){
+describe('Frontend test suite 1', function(){
 
     beforeEach(() => {
 
         cy.get(':nth-child(1) > input').type('tester01') //type in username
         cy.get(':nth-child(2) > input').type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c') //password
         cy.get('.btn').click() //Click log in
+    })
 
-      })
+        afterEach(() => {
+            cy.get('.user > .btn').click() //log out
+          })
+
+      
     // ett test case
-         it.skip('check title URL' , function(){
+         it('check title URL' , function(){
             cy.get(':nth-child(1) > .btn') .click()
             cy.get('h1 > .router-link-active') .click()
             cy.contains ('Tester Hotel Overview') //kolla så du är på rätt sida (home page)
-            cy.get('.user > .btn') .click() //logga ut
+            
 
          })
 
-         it.skip('edit client email' , function(){
+         it('edit client email' , function(){
             cy.get('.blocks > :nth-child(2) > .btn') .click()
             cy.get(':nth-child(1) > .action > img') .click() //edit/delete option
             cy.get('.menu > :nth-child(1)') .click() // click on edit
             cy.get(':nth-child(4) > input').clear() .type('Jonas.H@example.se') //clear old text and type in new text
             cy.get('.blue') .click() //save button
-            cy.get('.user > .btn') .click()
+            
             
             
 
          })
 
-         it.skip('go to edit room and press back' , function(){
+         it('go to edit room and press back' , function(){
             cy.get(':nth-child(1) > .btn').click()
             cy.get('h2 > .btn').click()
             cy.get('[href="/rooms"]').click() //back
             cy.contains('Rooms') //check the right page
-            cy.get('.user > .btn') .click()
+            
 
          })
 
-         it.skip('create reservation' , function(){
+         it('create reservation' , function(){
             cy.get(':nth-child(4) > .btn').click()
             cy.get('h2 > .btn').click()
+            cy.get(':nth-child(1) > input') .type('1995-09-09')
+            cy.get(':nth-child(2) > input') .type('1995-09-10')
             cy.get(':nth-child(3) > select').select('Jonas Hellman (#1)') //select option from dropdown
             cy.get(':nth-child(4) > select').select('Floor 1, Room 101')
             cy.get(':nth-child(5) > select').select('ID: 1')
@@ -57,17 +64,17 @@ describe('Frontend test suite', function(){
             
          })
 
-         it.skip('delete reservation' , function(){
+         it('delete reservation' , function(){
             cy.get(':nth-child(4) > .btn').click()
             cy.get('img').click() //edit/delete menu
             cy.get('.menu > :nth-child(2)').click()//click on delete
-            cy.contains('Reservations')
-            cy.get('.user > .btn') .click()
+            
+            
 
      
          })
 
-         it.skip('create new client - delete client' , function(){
+         it('create new client - delete client' , function(){
             cy.get('.blocks > :nth-child(2) > .btn') .click()
             cy.get('h2 > .btn').click() //create new 
             cy.get(':nth-child(1) > input') .type('Isabell Mouselli')
@@ -88,8 +95,6 @@ describe('Frontend test suite', function(){
             cy.get('.blue').click()//save
             cy.get(':nth-child(2) > .action > img').click()
             cy.get('.menu > :nth-child(2)').click()//delete
-
-
 
          })   
     })
