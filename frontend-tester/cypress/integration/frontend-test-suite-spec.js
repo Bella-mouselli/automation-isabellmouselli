c332f8536951d4569e6b473a3942425fc401e25d
Isabell Mouselli <bellam95@hotmail.com>
2021-11-01 13:01:54 +0100

    test cases before cleanup

--- frontend-tester/cypress/integration/frontend-test-suite-spec.js
+++ frontend-tester/cypress/integration/frontend-test-suite-spec.js
@@ -1,19 +1,14 @@
 /// <reference types="cypress" />
-// en test suite
+
+before(() => {
+
+    cy.visit('http://localhost:3000/%27')
+
+  })
+  // en test suite
 describe('Frontend test suite', function(){
     // ett test case
-        it('valid login', function(){
-            cy.visit('http://localhost:3000')
-            cy.contains('Login') 
-            cy.get(':nth-child(1) > input') .type('tester01')
-            cy.get(':nth-child(2) > input') .type ('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
-            cy.get('.btn') .click()
-            cy.contains ('Tester Hotel Overview')
-            cy.get('.user > .btn') .click()
-            cy.contains ('Login')
-         })
-
-         it('check title URL' , function(){
+         it.skip('check title URL' , function(){
             cy.get(':nth-child(1) > input') .type('tester01')
             cy.get(':nth-child(2) > input') .type ('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
             cy.get('.btn') .click()
@@ -24,7 +19,7 @@ describe('Frontend test suite', function(){
 
          })
 
-         it('edit client email' , function(){
+         it.skip('edit client email' , function(){
             cy.get(':nth-child(1) > input') .type('tester01')
             cy.get(':nth-child(2) > input') .type ('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
             cy.get('.btn') .click()
@@ -39,7 +34,7 @@ describe('Frontend test suite', function(){
 
          })
 
-         it('go to edit room and press back' , function(){
+         it.skip('go to edit room and press back' , function(){
             cy.get(':nth-child(1) > input') .type('tester01')
             cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
             cy.get('.btn') .click()
@@ -51,27 +46,63 @@ describe('Frontend test suite', function(){
 
          })
 
-         //it('delete reservation' , function(){
-           // cy.get(':nth-child(1) > input') .type('tester01')
-            //cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
-            //cy.get('.btn') .click()
-            //cy.get(':nth-child(4) > .btn').click()
-            //cy.get('img').click()
-            //cy.get('.menu > :nth-child(2)').click()
-            //cy.contains('Reservations')
-            //cy.get('.user > .btn') .click()
-            
-        // })
-
-         it('create reservation' , function(){
+         it.skip('create reservation' , function(){
             cy.get(':nth-child(1) > input') .type('tester01')
             cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
             cy.get('.btn') .click()
             cy.get(':nth-child(4) > .btn').click()
             cy.get('h2 > .btn').click()
-            cy.get('#app').select('Jonas Hellman')
-           
+            cy.get(':nth-child(3) > select').select('Jonas Hellman (#1)')
+            cy.get(':nth-child(4) > select').select('Floor 1, Room 101')
+            cy.get(':nth-child(5) > select').select('ID: 1')
+            cy.get('.blue').click()
+
+         
             
          })
-    
+
+         it.skip('delete reservation' , function(){
+            cy.get(':nth-child(1) > input') .type('tester01')
+            cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+            cy.get('.btn') .click()
+            cy.get(':nth-child(4) > .btn').click()
+            cy.get('img').click()
+            cy.get('.menu > :nth-child(2)').click()
+            cy.contains('Reservations')
+            cy.get('.user > .btn') .click()
+
+     
+         })
+
+         it.skip('create new client - delete client' , function(){
+            cy.get(':nth-child(1) > input') .type('tester01')
+            cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+            cy.get('.btn') .click()
+            cy.get('.blocks > :nth-child(2) > .btn') .click()
+            cy.get('h2 > .btn').click()
+            cy.get(':nth-child(1) > input') .type('Isabell Mouselli')
+            cy.get(':nth-child(2) > input').type('isabell@example.se')
+            cy.get(':nth-child(3) > input') .type('1111111111')
+            cy.get('.blue') .click()
+            cy.get(':nth-child(3) > .action > img').click()
+            cy.get('.menu > :nth-child(2)').click()
+
+
+         })
+
+         it('create new bill - delete bill' , function(){
+            cy.get(':nth-child(1) > input') .type('tester01')
+            cy.get(':nth-child(2) > input') .type('GteteqbQQgSr88SwNExUQv2ydb7xuf8c')
+            cy.get('.btn') .click()
+            cy.get('div.block:nth-child(3) > a:nth-child(4)').click()
+            cy.get('h2 > .btn').click()
+            cy.get('input').type('1500')
+            cy.get('.checkbox').click()
+            cy.get('.blue').click()
+            cy.get(':nth-child(2) > .action > img').click()
+            cy.get('.menu > :nth-child(2)').click()
+
+
+
+         })   
     })
